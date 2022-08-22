f9e96f45371da4cbca70f69d47f0df6d82a21fff
Vashishraja <109143077+Vashishraja@users.noreply.github.com>
2022-08-22 16:49:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+    for response in responses_data:
+        response_score = 0
+        required_score = 0
+        required_words = response['required_words']
+
+        if required_words:
+            for word in split_message:
+                if word in required_words:
+                    required_score +=1
+
+        if required_score == len(required_words):
+            for word in split_message:
+                if word in response["user_input"]:
+                    response_score += 1
+
+        score_list.append(response_score)
+
+    best_response = max(score_list)
+    response_index = score_list.index(best_response)
+
+    if input_string == "":
+        return"Please Type Something :)"
+
+    if best_response !=0:
+        return responses_data[response_index]["bot_response"]
+
+    return random_responses.random_string()
+
+
+while True:
+    user_input = input("You: ")
+    print("Bot:",get_response(user_input))
